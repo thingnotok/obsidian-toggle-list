c7ef422b6665d457a83db4eaa7e7da1be759f406
L.D. Chen <ldchen@gapp.nthu.edu.tw>
2022-10-03 15:01:08 +0800

    Update for suggestions

--- main.ts
+++ main.ts
@@ -99,16 +99,12 @@ function ToggleAction(editor: Editor, view: MarkdownView, setting: ToggleListSet
 			console.log('head with=' + r.offset)
 			if (selection.head.ch < -r.offset)
 				selection.head.ch = 0;
-			else if (selection.head.ch == 0)
-				selection.head.ch = 0;
 			else
 				selection.head.ch = selection.head.ch + r.offset;
 		}
 		if (i == anchor) {
 			console.log('anchor with=' + r.offset)
 			if (selection.anchor.ch < -r.offset)
-				selection.anchor.ch = 0;
-			else if (selection.anchor.ch == 0)
 				selection.anchor.ch = 0;
 			else
 				selection.anchor.ch = selection.anchor.ch + r.offset;
@@ -142,11 +138,6 @@ export default class ToggleList extends Plugin {
 		await this.loadSettings();
 
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'ToggleList-Next',
@@ -165,15 +156,6 @@ export default class ToggleList extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new ToggleListSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
