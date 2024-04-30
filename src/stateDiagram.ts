fbce1c298c32395ae9c4cd66a5bad695b29c571c
thingnotok <thingnotok@gmail.com>
2024-04-30 23:23:57 +0800

    Add cmd nodes

--- src/stateDiagram.ts
+++ src/stateDiagram.ts
@@ -3,7 +3,6 @@ import colormap from "colormap";
 import { ToggleListSettings } from "./settings";
 import { renderEmptyLine } from "src/tlAction";
 
-
 function genColorMap(samples: number) {
 	const num = samples < 256 ? 256 : samples;
 	const colors = colormap({
@@ -15,17 +14,19 @@ function genColorMap(samples: number) {
 	return colors;
 }
 
-function drawConnectionForSG(state_group: string | any[], statesAry: string | any[], cmdIdx: any){
+function drawConnectionForSG(
+	state_group: string | any[],
+	statesAry: string | any[],
+	cmdIdx: any
+) {
 	const edgeList = [];
 	const sgLen = state_group.length;
-	let fromIdx =0; let toIdx=0;
+	let fromIdx = 0;
+	let toIdx = 0;
 	for (let j = 0; j < sgLen - 1; j++) {
-		
-		for(let k=0;k<statesAry.length;k++){
-			if(state_group[j]==statesAry[k])
-				fromIdx = k
-			if(state_group[j+1]==statesAry[k])
-				toIdx = k
+		for (let k = 0; k < statesAry.length; k++) {
+			if (state_group[j] == statesAry[k]) fromIdx = k;
+			if (state_group[j + 1] == statesAry[k]) toIdx = k;
 		}
 		edgeList.push({
 			id: `edge${fromIdx}->${toIdx}`,
@@ -33,16 +34,15 @@ function drawConnectionForSG(state_group: string | any[], statesAry: string | an
 			fromSide: "bottom",
 			toNode: `${toIdx}`,
 			toSide: "top",
-			toEnd: 'arrow',
-			color: `${cmdIdx}`
-		})
+			toEnd: "arrow",
+			color: `${cmdIdx}`,
+		});
 	}
-	fromIdx =0; toIdx=0;
-	for(let k=0;k<statesAry.length;k++){
-		if(state_group[sgLen-1]==statesAry[k])
-			fromIdx = k
-		if(state_group[0]==statesAry[k])
-			toIdx = k
+	fromIdx = 0;
+	toIdx = 0;
+	for (let k = 0; k < statesAry.length; k++) {
+		if (state_group[sgLen - 1] == statesAry[k]) fromIdx = k;
+		if (state_group[0] == statesAry[k]) toIdx = k;
 	}
 	edgeList.push({
 		id: `edge${fromIdx}->${toIdx}`,
@@ -50,10 +50,10 @@ function drawConnectionForSG(state_group: string | any[], statesAry: string | an
 		fromSide: "bottom",
 		toNode: `${toIdx}`,
 		toSide: "left",
-		toEnd: 'arrow',
-		color: `${cmdIdx}`
-	})
-	return edgeList
+		toEnd: "arrow",
+		color: `${cmdIdx}`,
+	});
+	return edgeList;
 }
 
 export async function genDiagramCanvas(
@@ -62,6 +62,7 @@ export async function genDiagramCanvas(
 ): Promise<void> {
 	const colors = genColorMap(settings.cmd_list.length);
 	let transitions: string[] = [];
+	const cmdNodes = [];
 	for (let i = 0; i < settings.cmd_list.length; i++) {
 		const cmd = settings.cmd_list[i];
 		cmd.bindings.forEach((cmdIdx) => {
@@ -69,29 +70,46 @@ export async function genDiagramCanvas(
 				settings.setup_list[cmdIdx].states
 			);
 		});
+		cmdNodes.push({
+			id: `cmd${i}`,
+			x: i * 200,
+			y: -100,
+			width: 150,
+			height: 40,
+			type: "text",
+			text: cmd.name,
+			color: `${i}`
+		});
 	}
 	const statesSet = new Set(transitions);
-	const statesAry = Array.from(statesSet)
-	const nodes = statesAry.map((state, index) => ({
+	const statesAry = Array.from(statesSet);
+	const stateNodes = statesAry.map((state, index) => ({
 		id: `${index}`,
-		x: index*120,
+		x: index * 120,
 		y: index * 80,
 		width: 150,
 		height: 40,
 		type: "text",
 		text: state.trim(),
+		color: `${-1}`
 	}));
-	let edgeList:any[] = []
+	let edgeList: any[] = [];
 	for (let i = 0; i < settings.cmd_list.length; i++) {
 		const cmd = settings.cmd_list[i];
 		cmd.bindings.forEach((cmdIdx) => {
-			edgeList = edgeList.concat(drawConnectionForSG(settings.setup_list[cmdIdx].states, statesAry, i))
+			edgeList = edgeList.concat(
+				drawConnectionForSG(
+					settings.setup_list[cmdIdx].states,
+					statesAry,
+					i
+				)
+			);
 		});
 	}
 	await this.app.vault.create(
 		fileName,
 		JSON.stringify({
-			nodes: nodes,
+			nodes: cmdNodes.concat(stateNodes),
 			edges: edgeList,
 		})
 	);
